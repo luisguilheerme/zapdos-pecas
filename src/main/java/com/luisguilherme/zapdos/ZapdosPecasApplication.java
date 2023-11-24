2d36615b30135b530ab2cda59dee2e12f9faba70
luisguilheerme <lg.maroni@gmail.com>
2023-11-24 14:54:16 -0300

    Project created

---
+++ src/main/java/com/luisguilherme/zapdos/ZapdosPecasApplication.java
@@ -0,0 +1,13 @@
+package com.luisguilherme.zapdos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapdosPecasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZapdosPecasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luisguilherme/zapdos/ZapdosPecasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisguilherme.zapdos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZapdosPecasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
